0f4f2b636a2f078d40986e86b506fea1912900ae
larissa1998 <larissafogacaavare@hotmail.com>
2020-03-29 19:40:19 -0300

    Back-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,36 +7,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.listen(3333);
+
 /**
  * Rota/Recurso
  */
 
- /**
-  * Métodos HTTP
-  * GET : Buscar uma informação do back-end
-  * POST: Criar uma informação no back-end
-  * PUT: Alterar uma informação no back-end
-  * DELETE: Deletar uma informação no bacn-end
-  */
+/**
+ * Métodos HTTP
+ * GET : Buscar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no bacn-end
+ */
 
-  /**
-   * Tipos de Parâmetros:
-   * 
-   * Query: Parâmentros nomeados enviados na rota após "?" (Filtros, paginação)
-   * Route Params: Parâmetros utilizados para identificar recursos
-   * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-   */
+/**
+ * Tipos de Parâmetros:
+ * 
+ * Query: Parâmentros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
 
-   /**
-    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL sERVER
-    * NoSQL: MongoDB, CouchDB, etc
-    */
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL sERVER
+ * NoSQL: MongoDB, CouchDB, etc
+ */
 
-    /**
-     * Driver: SELECT * FROM users
-     * Qury Builder: table('users').select('*').where()
-     */
-
-
-
-app.listen(3333);
+/**
+ * Driver: SELECT * FROM users
+ * Qury Builder: table('users').select('*').where()
+ */
